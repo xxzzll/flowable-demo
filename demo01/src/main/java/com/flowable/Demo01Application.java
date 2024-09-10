f977d1a568c3491e2ab11cd1c5f33b6697a17122
xixi <lzx20181111@163.com>
2024-09-10 23:30:24 +0800

    comment1

--- demo01/src/main/java/com/flowable/Demo01Application.java
+++ demo01/src/main/java/com/flowable/Demo01Application.java
@@ -3,6 +3,9 @@ package com.flowable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * comment1
+ */
 @SpringBootApplication
 public class Demo01Application {
 
