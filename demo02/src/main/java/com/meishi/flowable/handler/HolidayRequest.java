920f32632901865db00446f914b7d17058373df3
xixi <lzx20181111@163.com>
2024-09-10 23:36:25 +0800

    master(change1)

--- demo02/src/main/java/com/meishi/flowable/handler/HolidayRequest.java
+++ demo02/src/main/java/com/meishi/flowable/handler/HolidayRequest.java
@@ -6,8 +6,6 @@ import org.flowable.engine.impl.cfg.StandaloneProcessEngineConfiguration;
 
 /**
  * @author lizhenxi
- * @create 2020-05-29-16:31
- * @description
  */
 public class HolidayRequest {
 
